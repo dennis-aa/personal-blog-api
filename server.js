bc795c8527969a95adb02ae2993bfcbdf9ea94b8
Dennis Arias <dennisarias1996@yahoo.com>
2023-09-14 20:23:49 -0400

    Setup environment vars and connected to hosted DB

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log("DB Connection Successful");
+});
 //Setup port to listen on
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
