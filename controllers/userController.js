9064f604216b463145e07854f20e6a1c51e1c9c4
Dennis Arias <dennisarias1996@yahoo.com>
2023-09-29 12:03:28 -0400

    Implemented user sign up and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,19 @@
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+
 //Place holders for when we actually add users
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "this route is not yet defined",
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  //SEND RESPONSE
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
