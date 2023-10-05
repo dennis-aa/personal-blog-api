72de4bc24c854b770c74df42d2dbe5fe4219473b
Dennis Arias <dennisarias1996@yahoo.com>
2023-10-05 11:53:29 -0400

    Added sending tokens via cookie & rate limiting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //Imports
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,10 +13,18 @@ const userRouter = require("./routes/userRoutes");
 //Initialize Express
 const app = express();
 
-//Using third-party Middleware
-app.use(morgan("dev"));
+//GLOBAL MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-//Setup Middleware
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour",
+});
+
+app.use("/api", limiter);
 
 app.use(express.json());
 
